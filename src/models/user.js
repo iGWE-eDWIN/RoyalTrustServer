700d2486dd67dc33a2256b845db71adc7c9b8053
iGWE-eDWIN <igweedwin15@gmail.com>
2025-06-06 12:26:00 +0100

    second commit

--- src/models/user.js
+++ src/models/user.js
@@ -108,7 +108,7 @@ userSchema.methods.generateAuthToken = async function () {
   const user = this;
   const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_Secret);
   user.tokens = user.tokens.concat({ token });
-  await user.save();
+  // await user.save();
 
   return token;
 };
